1aa171272c4845b44f613bafd7ea0c48930ac865
Shauna Mack <shaunakatemack@gmail.com>
2020-04-10 22:38:19 +1000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum age: ["18-23", "24-37", "38-56", "57-64", "65+" ]
+
+end
